0065d4bb99926749f6e86f4a9d93a7eabf696073
simengangstad <simen.gangstad@me.com>
2015-11-06 22:14:28 +0000

    Initial commit

---
+++ desktop/src/io/github/simengangstad/defendthecaves/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.simengangstad.defendthecaves.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.simengangstad.defendthecaves.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
